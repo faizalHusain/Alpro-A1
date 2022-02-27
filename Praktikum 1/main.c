beef7de57d5ec577e26e398efa5a6b6a82a9aa9f
Faizal_Husain_Adiasha <88271988+faizalHusain@users.noreply.github.com>
2022-02-27 07:09:02 +0700

    Add files via upload

---
+++ Praktikum 1/main.c
@@ -0,0 +1,13 @@
+
+// Nama         : Faizal Husain Adiasha
+// NIM          : 24060121140115
+// Nama file    : main.c
+// Deskripsi    : Program hello world
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
