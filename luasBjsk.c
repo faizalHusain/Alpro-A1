5d7e167e0e16983c18c4b18787450ff406195f65
Faizal_Husain_Adiasha <88271988+faizalHusain@users.noreply.github.com>
2022-03-07 23:01:34 +0700

    Add files via upload

---
+++ luasBjsk.c
@@ -0,0 +1,20 @@
+// Nama : Faizal Husain Adiasha
+// NIM	: 24060121140115
+#include <stdio.h>
+#include <stdlib.h>
+
+//Program luasBjsk
+//{Menghitung dan menampilkan luas bujursangkar di layar} 
+
+
+
+int main() {
+	//Kamus
+	int s,luas;
+	//Algoritma
+	printf("Masukkan sisi: ");scanf("%d",&s);
+	luas = s*s;
+	printf("\nLuas Bujur sangkar		: %d", luas);
+	
+	return 0;
+}
