0cfc7b5861f26bcad3bb4a0c11a9120f4b090013
Faizal_Husain_Adiasha <88271988+faizalHusain@users.noreply.github.com>
2022-03-22 05:30:44 +0700

    Add files via upload

---
+++ Praktikum 4/CekPrima.c
@@ -0,0 +1,28 @@
+// Nama File	: CekPrima.c
+// Deskripsi	: mengecek suatu bilangan apakah bilangan prima
+// Pembuat		: Faizal Husain Adiasha - 24060121140115
+// Tgl Pembuatan: Minggu, 20 Maret 2022, 20.00 WIB
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	//Kamus
+	int i,N,jmlFaktor;
+	//Algoritma
+	printf("Masukkan Bilangan: ");scanf("%d",&N);
+	if (N>0){
+		for (i=1;i<=N;i++){
+			if ((N % i) == 0){
+				jmlFaktor += 1;
+			}
+		}
+		if (jmlFaktor==2){
+			printf("Bilangan Prima");
+		}
+		else {
+			printf("Bukan bilangan Prima");
+		}	
+	}else {
+			printf("Masukan Harus Lebih dari nol");
+	}
+	return 0;
+}
